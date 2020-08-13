d7753c5f6c511c8fad462bd51998e2e9870c01bf
fepz <franpaez+github@gmail.com>
2020-08-13 20:13:15 -0300

    Generate books list.

---
+++ books/books.md
@@ -0,0 +1,19 @@
+# Libros:
+- Stephen King. *Buick 8: un coche perverso*. Sudamericana, 2010.
+
+- Isaac Asimov. *Cuentos Completos I*. Ediciones B, 2008.
+
+- Isaac Asimov. *Cuentos Completos II*. Ediciones B, 2008.
+
+- Frank Herbert. *Dune*. Debolsillo, 2013.
+
+- Stephen King. *El resplandor*. Debolsillo, 2014.
+
+- Simon Singh. *El último teorema de Fermat*. Páprika, 2014.
+
+- Stephen King. *La Niebla*. Sudamericana, 2010.
+
+- Thomas S. Kuhn. *The structure of scientific revolutions*. The University of Chicago Press, 2012.
+
+- Alan F. Chalmers. *¿Qué es es cosa llamada ciencia?*. Siglo veintiuno editores, 1995.
+
---
+++ books/books.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlopen
+from tqdm.auto import tqdm  
+from argparse import ArgumentParser
+import json
+import pandas
+import numpy as np
+
+
+def get_args():
+    parser = ArgumentParser()
+    parser.add_argument("--update", action="store_true", help="Update.")
+    return parser.parse_args()
+
+def main():
+    args = get_args()
+    with open('books.json', "r") as f:
+        print('# Libros:')
+        for book in sorted(json.load(f), key=lambda k: k["title"]):
+            print("- {0:}. *{1:}*. {2:}, {3:}.\n".format(book["author"],
+                book["title"], book["editorial"], book["year"]))
+
+if __name__ == '__main__':
+    main()
+
